d56cfe5b5e751213cbe3ce579cb6182cfeb32c99
Danilo <didox_59@yahoo.com.br>
2018-09-22 16:44:05 -0300

    multiclube

--- app/controllers/multiclube_controller.rb
+++ app/controllers/multiclube_controller.rb
@@ -6,7 +6,7 @@ class MulticlubeController < ActionController::Base
       if usuarios.count > 0
         usuario = usuarios.first
       else
-        usuario = Usuario.create(nome: params[:telefone], cpf: params[:cpf], email: params[:email], telefone: params[:telefone])
+        usuario = Usuario.create(nome: params[:nome], cpf: params[:cpf], email: params[:email], telefone: params[:telefone])
       end
 
       cookies[:usuario_multiclube] = { value: usuario.id, expires: 1.day.from_now, httponly: true }
